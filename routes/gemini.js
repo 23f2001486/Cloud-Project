601a855803bef5a4390c155388805c9c69191f3e
Divya P <23f2001486@ds.study.iitm.ac.in>
2025-11-06 15:14:26 +0530

    Update gemini.js

--- routes/gemini.js
+++ routes/gemini.js
@@ -1,9 +1,9 @@
 import express from "express";
 import {  getComplaintsBySensitivity } from "../controllers/gemini.js";
-
+import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/gemini",  getComplaintsBySensitivity);
+router.get("/gemini", verifyToken, getComplaintsBySensitivity);
 
 export default router;
