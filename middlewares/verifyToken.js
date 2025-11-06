9ffafcf0561eab0fe0b7e8d44c82e90f6b569f92
Divya P <23f2001486@ds.study.iitm.ac.in>
2025-11-06 16:43:04 +0530

    Update verifyToken.js

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -14,6 +14,7 @@ export const verifyToken = (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // attach decoded payload (user data)
     console.log("User decoded:", req.user);
+    console.log(req.user._id);
     next();
   } catch (error) {
     res.status(400).json({ message: "Invalid token." });
