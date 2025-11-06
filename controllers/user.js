c86fd734fcb3bdb38ce8e2e8401ea3d979520495
Divya P <23f2001486@ds.study.iitm.ac.in>
2025-11-06 21:31:03 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,7 @@ import User from "../models/user-model.js";
 
 export const getUserProfile = async (req, res) => {
   try {
+    console.log(req);
     const { id } = req.user.id;
     console.log(id);// user ID from URL or from auth token
 
