efb08210a99d603eb400f53f1ea6126b4109a9b2
Divya P <23f2001486@ds.study.iitm.ac.in>
2025-11-06 21:12:01 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ import { verifyToken } from "../middlewares/verifyToken.js"; // ✅ import JWT m
 const router = express.Router();
 
 // ✅ Secure user profile route
-router.get("/:id", verifyToken, getUserProfile);
+router.get("", verifyToken, getUserProfile);
 
 export default router;
 
